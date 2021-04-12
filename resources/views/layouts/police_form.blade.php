a29a1b59acdf29e33bc1b5eca1a207d2ca1a4cac
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-04-12 15:00:05 +0300

    Production build

--- resources/views/layouts/police_form.blade.php
+++ resources/views/layouts/police_form.blade.php
@@ -12,7 +12,9 @@
     <!-- Favicon -->
     <link href="{{asset('favicon.svg')}}" rel="icon" type="image/png">
     <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Icons -->
     <link href="{{ asset('argon/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
     <link href="{{ asset('argon/vendor/@fortawesome/fontawesome-free/css/fontawesome.min.css') }}" rel="stylesheet">
